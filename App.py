b24ce485aa61f423bd81ca9d3cc55983d433f4e9
Kushal Bhavsar <kushalbhavsar58@gmail.com>
2022-01-30 08:48:50 +0530

    Initial commit

---
+++ App.py
@@ -0,0 +1,166 @@
+import streamlit as st
+from PIL import Image
+import json
+from Classifier import KNearestNeighbours
+from bs4 import BeautifulSoup
+import requests,io
+import PIL.Image
+from urllib.request import urlopen
+
+with open('./Data/movie_data.json', 'r+', encoding='utf-8') as f:
+    data = json.load(f)
+with open('./Data/movie_titles.json', 'r+', encoding='utf-8') as f:
+    movie_titles = json.load(f)
+
+def movie_poster_fetcher(imdb_link):
+    ## Display Movie Poster
+    url_data = requests.get(imdb_link).text
+    s_data = BeautifulSoup(url_data, 'html.parser')
+    imdb_dp = s_data.find("meta", property="og:image")
+    movie_poster_link = imdb_dp.attrs['content']
+    u = urlopen(movie_poster_link)
+    raw_data = u.read()
+    image = PIL.Image.open(io.BytesIO(raw_data))
+    image = image.resize((158, 301), )
+    st.image(image, use_column_width=False)
+
+def get_movie_info(imdb_link):
+    url_data = requests.get(imdb_link).text
+    s_data = BeautifulSoup(url_data, 'html.parser')
+    imdb_content = s_data.find("meta", property="og:description")
+    movie_descr = imdb_content.attrs['content']
+    movie_descr = str(movie_descr).split('.')
+    movie_director = movie_descr[0]
+    movie_cast = str(movie_descr[1]).replace('With', 'Cast: ').strip()
+    movie_story = 'Story: ' + str(movie_descr[2]).strip()+'.'
+    rating = s_data.find("div", class_="AggregateRatingButton__TotalRatingAmount-sc-1ll29m0-3 jkCVKJ")
+    rating = str(rating).split('<div class="AggregateRatingButton__TotalRatingAmount-sc-1ll29m0-3 jkCVKJ')
+    rating = str(rating[1]).split("</div>")
+    rating = str(rating[0]).replace(''' "> ''', '').replace('">', '')
+
+    movie_rating = 'Total Rating count: '+ rating
+    return movie_director,movie_cast,movie_story,movie_rating
+
+def KNN_Movie_Recommender(test_point, k):
+    # Create dummy target variable for the KNN Classifier
+    target = [0 for item in movie_titles]
+    # Instantiate object for the Classifier
+    model = KNearestNeighbours(data, target, test_point, k=k)
+    # Run the algorithm
+    model.fit()
+    # Print list of 10 recommendations < Change value of k for a different number >
+    table = []
+    for i in model.indices:
+        # Returns back movie title and imdb link
+        table.append([movie_titles[i][0], movie_titles[i][2],data[i][-1]])
+    print(table)
+    return table
+
+st.set_page_config(
+   page_title="Movie Recommender System",
+)
+
+def run():
+    img1 = Image.open('./meta/logo.jpg')
+    img1 = img1.resize((250,250),)
+    st.image(img1,use_column_width=False)
+    st.title("Movie Recommender System")
+    st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Data is based "IMDB 5000 Movie Dataset"</h4>''',
+                unsafe_allow_html=True)
+    genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family',
+              'Fantasy', 'Film-Noir', 'Game-Show', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News',
+              'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
+    movies = [title[0] for title in movie_titles]
+    category = ['--Select--', 'Movie based', 'Genre based']
+    cat_op = st.selectbox('Select Recommendation Type', category)
+    if cat_op == category[0]:
+        st.warning('Please select Recommendation Type!!')
+    elif cat_op == category[1]:
+        select_movie = st.selectbox('Select movie: (Recommendation will be based on this selection)', ['--Select--'] + movies)
+        dec = st.radio("Want to Fetch Movie Poster?", ('Yes', 'No'))
+        st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Fetching a Movie Posters will take a time."</h4>''',
+                    unsafe_allow_html=True)
+        if dec == 'No':
+            if select_movie == '--Select--':
+                st.warning('Please select Movie!!')
+            else:
+                no_of_reco = st.slider('Number of movies you want Recommended:', min_value=5, max_value=20, step=1)
+                genres = data[movies.index(select_movie)]
+                test_points = genres
+                table = KNN_Movie_Recommender(test_points, no_of_reco+1)
+                table.pop(0)
+                c = 0
+                st.success('Some of the movies from our Recommendation, have a look below')
+                for movie, link, ratings in table:
+                    c+=1
+                    director,cast,story,total_rat = get_movie_info(link)
+                    st.markdown(f"({c})[ {movie}]({link})")
+                    st.markdown(director)
+                    st.markdown(cast)
+                    st.markdown(story)
+                    st.markdown(total_rat)
+                    st.markdown('IMDB Rating: ' + str(ratings) + '⭐')
+        else:
+            if select_movie == '--Select--':
+                st.warning('Please select Movie!!')
+            else:
+                no_of_reco = st.slider('Number of movies you want Recommended:', min_value=5, max_value=20, step=1)
+                genres = data[movies.index(select_movie)]
+                test_points = genres
+                table = KNN_Movie_Recommender(test_points, no_of_reco+1)
+                table.pop(0)
+                c = 0
+                st.success('Some of the movies from our Recommendation, have a look below')
+                for movie, link, ratings in table:
+                    c += 1
+                    st.markdown(f"({c})[ {movie}]({link})")
+                    movie_poster_fetcher(link)
+                    director,cast,story,total_rat = get_movie_info(link)
+                    st.markdown(director)
+                    st.markdown(cast)
+                    st.markdown(story)
+                    st.markdown(total_rat)
+                    st.markdown('IMDB Rating: ' + str(ratings) + '⭐')
+    elif cat_op == category[2]:
+        sel_gen = st.multiselect('Select Genres:', genres)
+        dec = st.radio("Want to Fetch Movie Poster?", ('Yes', 'No'))
+        st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Fetching a Movie Posters will take a time."</h4>''',
+                    unsafe_allow_html=True)
+        if dec == 'No':
+            if sel_gen:
+                imdb_score = st.slider('Choose IMDb score:', 1, 10, 8)
+                no_of_reco = st.number_input('Number of movies:', min_value=5, max_value=20, step=1)
+                test_point = [1 if genre in sel_gen else 0 for genre in genres]
+                test_point.append(imdb_score)
+                table = KNN_Movie_Recommender(test_point, no_of_reco)
+                c = 0
+                st.success('Some of the movies from our Recommendation, have a look below')
+                for movie, link, ratings in table:
+                    c += 1
+                    st.markdown(f"({c})[ {movie}]({link})")
+                    director,cast,story,total_rat = get_movie_info(link)
+                    st.markdown(director)
+                    st.markdown(cast)
+                    st.markdown(story)
+                    st.markdown(total_rat)
+                    st.markdown('IMDB Rating: ' + str(ratings) + '⭐')
+        else:
+            if sel_gen:
+                imdb_score = st.slider('Choose IMDb score:', 1, 10, 8)
+                no_of_reco = st.number_input('Number of movies:', min_value=5, max_value=20, step=1)
+                test_point = [1 if genre in sel_gen else 0 for genre in genres]
+                test_point.append(imdb_score)
+                table = KNN_Movie_Recommender(test_point, no_of_reco)
+                c = 0
+                st.success('Some of the movies from our Recommendation, have a look below')
+                for movie, link, ratings in table:
+                    c += 1
+                    st.markdown(f"({c})[ {movie}]({link})")
+                    movie_poster_fetcher(link)
+                    director,cast,story,total_rat = get_movie_info(link)
+                    st.markdown(director)
+                    st.markdown(cast)
+                    st.markdown(story)
+                    st.markdown(total_rat)
+                    st.markdown('IMDB Rating: ' + str(ratings) + '⭐')
+run()
---
+++ README.md
@@ -0,0 +1,47 @@
+# Movie Recommendation🍿 System
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)                 
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)   
+
+## [Watch Tutorial for this project]()
+<img src="https://github.com/Spidy20/LandMark_Detection/blob/master/yt_thumb.jpg">
+
+## Source
+- [IMDB Movie 5000 Dataset](https://www.kaggle.com/carolzhangdc/imdb-5000-movie-dataset)
+- Inspired from this [original repository](https://github.com/ppai22/knn_movie_recommender).
+
+## Features
+- Simple responsive UI
+- Movie Story
+- Movie Posters
+- Directors & Cast information
+- Total ratings
+- IMDB Ratings⭐
+
+## Usage
+
+- Clone my repository.
+- Open CMD in working directory.
+- Run following command.
+  ```
+  pip install -r requirements.txt
+  ```
+- `App.py` is the main Python file of Streamlit Web-Application. 
+- To run app, write following command in CMD. or use any IDE.
+  ```
+  streamlit run App.py
+  ```
+- `Movie_Data_Processing.ipynb` is the notebook of data processing.
+- `Classifier.py` is the main file which is containing a KNN Algorithm.
+- For more explanation of this project see the tutorial on Machine Learning Hub YouTube channel.
+
+## Screenshots
+
+<img src="https://github.com/Spidy20/LandMark_Detection/blob/master/s1.PNG">
+<img src="https://github.com/Spidy20/LandMark_Detection/blob/master/s2.PNG">
+
+
+## Just follow☝️ me and Star⭐ my repository 
+
+# [Buy me a Coffee☕](https://www.buymeacoffee.com/spidy20)
+## [Donate me on PayPal(It will inspire me to do more projects)](https://www.paypal.me/spidy1820)
