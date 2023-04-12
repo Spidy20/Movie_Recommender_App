b0b6557422cefc57be1d073ab1134bef08cd0cab
Kushal Bhavsar <kushalbhavsar58@gmail.com>
2023-04-12 21:16:49 +0530

    Updated App

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ from PIL import Image
 import json
 from Classifier import KNearestNeighbours
 from bs4 import BeautifulSoup
-import requests, io
+import requests,io
 import PIL.Image
 from urllib.request import urlopen
 
@@ -11,35 +11,31 @@ with open('./Data/movie_data.json', 'r+', encoding='utf-8') as f:
     data = json.load(f)
 with open('./Data/movie_titles.json', 'r+', encoding='utf-8') as f:
     movie_titles = json.load(f)
-hdr = {'User-Agent': 'Mozilla/5.0'}
 
-
-def movie_poster_fetcher(imdb_link):
-    ## Display Movie Poster
-    url_data = requests.get(imdb_link, headers=hdr).text
-    s_data = BeautifulSoup(url_data, 'html.parser')
-    imdb_dp = s_data.find("meta", property="og:image")
-    movie_poster_link = imdb_dp.attrs['content']
-    u = urlopen(movie_poster_link)
-    raw_data = u.read()
-    image = PIL.Image.open(io.BytesIO(raw_data))
-    image = image.resize((158, 301), )
-    st.image(image, use_column_width=False)
-
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
 def get_movie_info(imdb_link):
-    url_data = requests.get(imdb_link, headers=hdr).text
+    url_data = requests.get(imdb_link).text
     s_data = BeautifulSoup(url_data, 'html.parser')
     imdb_content = s_data.find("meta", property="og:description")
     movie_descr = imdb_content.attrs['content']
     movie_descr = str(movie_descr).split('.')
     movie_director = movie_descr[0]
     movie_cast = str(movie_descr[1]).replace('With', 'Cast: ').strip()
-    movie_story = 'Story: ' + str(movie_descr[2]).strip() + '.'
-    rating = s_data.find("span", class_="sc-bde20123-1 iZlgcd").text
-    movie_rating = 'Total Rating count: ' + str(rating)
-    return movie_director, movie_cast, movie_story, movie_rating
+    movie_story = 'Story: ' + str(movie_descr[2]).strip()+'.'
+    rating = s_data.find("div", class_="AggregateRatingButton__TotalRatingAmount-sc-1ll29m0-3 jkCVKJ")
+    rating = str(rating).split('<div class="AggregateRatingButton__TotalRatingAmount-sc-1ll29m0-3 jkCVKJ')
+    rating = str(rating[1]).split("</div>")
+    rating = str(rating[0]).replace(''' "> ''', '').replace('">', '')
 
+    movie_rating = 'Total Rating count: '+ rating
+    return movie_director,movie_cast,movie_story,movie_rating
 
 def KNN_Movie_Recommender(test_point, k):
     # Create dummy target variable for the KNN Classifier
@@ -52,20 +48,18 @@ def KNN_Movie_Recommender(test_point, k):
     table = []
     for i in model.indices:
         # Returns back movie title and imdb link
-        table.append([movie_titles[i][0], movie_titles[i][2], data[i][-1]])
+        table.append([movie_titles[i][0], movie_titles[i][2],data[i][-1]])
     print(table)
     return table
 
-
 st.set_page_config(
-    page_title="Movie Recommender System",
+   page_title="Movie Recommender System",
 )
-
 
 def run():
     img1 = Image.open('./meta/logo.jpg')
-    img1 = img1.resize((250, 250), )
-    st.image(img1, use_column_width=False)
+    img1 = img1.resize((250,250),)
+    st.image(img1,use_column_width=False)
     st.title("Movie Recommender System")
     st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Data is based "IMDB 5000 Movie Dataset"</h4>''',
                 unsafe_allow_html=True)
@@ -78,12 +72,10 @@ def run():
     if cat_op == category[0]:
         st.warning('Please select Recommendation Type!!')
     elif cat_op == category[1]:
-        select_movie = st.selectbox('Select movie: (Recommendation will be based on this selection)',
-                                    ['--Select--'] + movies)
+        select_movie = st.selectbox('Select movie: (Recommendation will be based on this selection)', ['--Select--'] + movies)
         dec = st.radio("Want to Fetch Movie Poster?", ('Yes', 'No'))
-        st.markdown(
-            '''<h4 style='text-align: left; color: #d73b5c;'>* Fetching a Movie Posters will take a time."</h4>''',
-            unsafe_allow_html=True)
+        st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Fetching a Movie Posters will take a time."</h4>''',
+                    unsafe_allow_html=True)
         if dec == 'No':
             if select_movie == '--Select--':
                 st.warning('Please select Movie!!')
@@ -91,13 +83,13 @@ def run():
                 no_of_reco = st.slider('Number of movies you want Recommended:', min_value=5, max_value=20, step=1)
                 genres = data[movies.index(select_movie)]
                 test_points = genres
-                table = KNN_Movie_Recommender(test_points, no_of_reco + 1)
+                table = KNN_Movie_Recommender(test_points, no_of_reco+1)
                 table.pop(0)
                 c = 0
                 st.success('Some of the movies from our Recommendation, have a look below')
                 for movie, link, ratings in table:
-                    c += 1
-                    director, cast, story, total_rat = get_movie_info(link)
+                    c+=1
+                    director,cast,story,total_rat = get_movie_info(link)
                     st.markdown(f"({c})[ {movie}]({link})")
                     st.markdown(director)
                     st.markdown(cast)
@@ -111,15 +103,15 @@ def run():
                 no_of_reco = st.slider('Number of movies you want Recommended:', min_value=5, max_value=20, step=1)
                 genres = data[movies.index(select_movie)]
                 test_points = genres
-                table = KNN_Movie_Recommender(test_points, no_of_reco + 1)
+                table = KNN_Movie_Recommender(test_points, no_of_reco+1)
                 table.pop(0)
                 c = 0
                 st.success('Some of the movies from our Recommendation, have a look below')
                 for movie, link, ratings in table:
                     c += 1
                     st.markdown(f"({c})[ {movie}]({link})")
-                    movie_poster_fetcher(link)
-                    director, cast, story, total_rat = get_movie_info(link)
+                    
+                    director,cast,story,total_rat = get_movie_info(link)
                     st.markdown(director)
                     st.markdown(cast)
                     st.markdown(story)
@@ -128,9 +120,8 @@ def run():
     elif cat_op == category[2]:
         sel_gen = st.multiselect('Select Genres:', genres)
         dec = st.radio("Want to Fetch Movie Poster?", ('Yes', 'No'))
-        st.markdown(
-            '''<h4 style='text-align: left; color: #d73b5c;'>* Fetching a Movie Posters will take a time."</h4>''',
-            unsafe_allow_html=True)
+        st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Fetching a Movie Posters will take a time."</h4>''',
+                    unsafe_allow_html=True)
         if dec == 'No':
             if sel_gen:
                 imdb_score = st.slider('Choose IMDb score:', 1, 10, 8)
@@ -143,7 +134,7 @@ def run():
                 for movie, link, ratings in table:
                     c += 1
                     st.markdown(f"({c})[ {movie}]({link})")
-                    director, cast, story, total_rat = get_movie_info(link)
+                    director,cast,story,total_rat = get_movie_info(link)
                     st.markdown(director)
                     st.markdown(cast)
                     st.markdown(story)
@@ -161,13 +152,11 @@ def run():
                 for movie, link, ratings in table:
                     c += 1
                     st.markdown(f"({c})[ {movie}]({link})")
-                    movie_poster_fetcher(link)
-                    director, cast, story, total_rat = get_movie_info(link)
+                   
+                    director,cast,story,total_rat = get_movie_info(link)
                     st.markdown(director)
                     st.markdown(cast)
                     st.markdown(story)
                     st.markdown(total_rat)
                     st.markdown('IMDB Rating: ' + str(ratings) + '⭐')
-
-
 run()
